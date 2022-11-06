9c0eebb047af5657eb9bde883e0287dac64f8cd6
OkashaTanoli <okashatanoli12345@gmail.com>
2022-11-06 22:41:19 +0500

    abc

---
+++ index.js
@@ -0,0 +1,53 @@
+// console.log("hello world")
+
+// var _name = "okasha";
+// console.log(_name)
+
+
+// let _name = "okasha" 
+// const age = 10;
+// _name = "fahad"
+// console.log(_name);
+
+// if(true){
+//     let _name = "abc";
+// }
+
+// console.log(_name)
+
+
+
+// let name = "okasha"
+// let age = 19
+// let married  = false
+// let abc = null;
+// console.log(abc)
+
+
+// let a = Symbol("abc")
+// let b = Symbol("abc")
+// console.log(a === b )
+
+
+
+// let _name = "okasha"
+// _name = 10
+// _name  = false
+// console.log(_name)
+
+
+
+
+// concatenation
+
+// let a = 10
+// let b = 2
+// let c = a+b
+// console.log(c)
+// console.log(typeof c)
+
+
+const SS = require("./sumsub")
+console.log(SS.Sub(10,2))
+console.log(SS.Sum(10,2))
+console.log(SS.a)
---
+++ sumsub.js
@@ -0,0 +1,11 @@
+function Sum(a, b) {
+    return a + b
+}
+
+
+function Sub(a, b) {
+    return a - b;
+}
+let a = 10
+
+module.exports = { Sum, Sub, a }
